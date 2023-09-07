d6489e3fb5e2133be784ac8b79c41ddd999cf946
Damian <damian.lachenmayr@tum.de>
2023-09-07 14:39:04 +0200

    adjust playpos in steering

--- UnderMined_URP/Assets/Features/Interactables/SteeringWheel.cs
+++ UnderMined_URP/Assets/Features/Interactables/SteeringWheel.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class SteeringWheel : Interactable
 {
     [SerializeField] public DrillController drill;
-    
+    [SerializeField] private Vector3 playerOffset = new Vector3(0, .81f, 1.0f);
+
     public override void Interact(CharacterInputController player)
     {
         if(!player.pickUp && drill.isRunning)
@@ -15,6 +16,8 @@ public class SteeringWheel : Interactable
             player._drillController = drill;
             player.transform.position = this.gameObject.transform.position - (this.gameObject.transform.forward);
             player.transform.SetParent(drill.gameObject.transform);
+            player.transform.position = player.transform.position - playerOffset;
+            
             player.currentInteractable = null;
         }
     }
