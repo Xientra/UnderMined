1658e4e66f56eef46989b20c3dabc46d114968a2
Damian-Lachenmayr <damian.lachenmayr@tum.de>
2025-07-28 16:33:02 +0200

    improved interactable system

--- UnderMined_URP/Assets/Features/Interactables/Interactable.cs
+++ UnderMined_URP/Assets/Features/Interactables/Interactable.cs
@@ -1,10 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public abstract class Interactable : MonoBehaviour
 {
-
     
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<CharacterInputController>().currentInteractable = this;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (other.GetComponent<CharacterInputController>().currentInteractable == this)
+            {
+                other.GetComponent<CharacterInputController>().currentInteractable = null;
+            }
+        }
+    }
+
     public abstract void Interact( CharacterInputController player);
 }
